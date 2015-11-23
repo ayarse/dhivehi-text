4d448b6e806d6edae8b342eda1205584ba33b0f0
Arushad Ahmed <contact@arushad.org>
2015-11-23 22:32:12 +0800

    Initial commit

---
+++ dhivehi-text.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name: Dhivehi Text
+Plugin URI: http://wordpress.org/extend/plugins/dhivehi-text/
+Description: Adds a button to the WordPress TinyMCE editor for Dhivehi text.
+Version: 0.1
+Author: Arushad Ahmed (@dash8x)
+Author URI: http://arushad.org
+Text Domain: dhivehi-text
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+
+/*------------------------------------*\
+    Actions + Filters + ShortCodes
+\*------------------------------------*/

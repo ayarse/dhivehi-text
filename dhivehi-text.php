952058ec91ce53f6f80c5b210fe67f945f76f4aa
Arushad Ahmed <contact@arushad.org>
2015-11-24 01:32:44 +0800

    Created TinyMCE js

--- dhivehi-text.php
+++ dhivehi-text.php
@@ -13,6 +13,17 @@ Text Domain: dhivehi-text
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 /**
+ * Adds stylesheets and scripts to WordPress dashboard
+ *
+ * @param void
+ * @return void
+ * @since 0.1
+ */
+function dhivehi_text_admin_scripts() {
+    wp_enqueue_style( 'dhivehi-text' );
+}
+
+/**
  * Adds the [dhivehi] shortcode button to TinyMCE
  *
  * @param array $plugin_array
@@ -35,6 +46,29 @@ function dhivehi_text_register_buttons( $buttons ) {
     array_push( $buttons, 'dhivehi' );
     return $buttons;
 }
+
+/**
+ * Add styles to the WYSIWYG editor
+ *
+ * @param void
+ * @return void
+ * @since  0.1
+ */
+function dhivehi_text_editor_styles() {
+    add_editor_style( plugins_url('/css/dhivehi-text.css', __FILE__) );
+}
+
+/**
+ * Registers the plugin styles
+ *
+ * @param void
+ * @return void
+ * @since  0.1
+ */
+function dhivehi_text_register_styles() {
+    wp_register_style('dhivehi-text', plugins_url('/css/dhivehi-text.css', __FILE__), array(), '0.1', 'all');
+}
+
 /**
  * Enqueue the plugin styles
  *
@@ -43,7 +77,6 @@ function dhivehi_text_register_buttons( $buttons ) {
  * @since  0.1
  */
 function dhivehi_text_styles() {
-    wp_register_style('dhivehi-text', plugins_url('/css/dhivehi-text.css', __FILE__), array(), '0.1', 'all');
     wp_enqueue_style('dhivehi-text');
 }
 
@@ -82,11 +115,14 @@ function dhivehi_text_shortcode($atts, $content=null) {
     Actions + Filters + ShortCodes
 \*------------------------------------*/
 // Add Actions
-add_action('wp_enqueue_scripts', 'dhivehi_text_styles'); // Add Plugin Stylesheet
+add_action( 'init', 'dhivehi_text_register_styles' ); //Register styles
+add_action( 'wp_enqueue_scripts', 'dhivehi_text_styles' ); // Add plugin stylesheet
+add_action( 'admin_enqueue_scripts', 'dhivehi_text_admin_scripts' ); //Admin scripts
+add_action( 'init', 'dhivehi_text_editor_styles' ); //Editor styles
 
 //Add Filters
 add_filter( 'mce_external_plugins', 'dhivehi_text_add_buttons' ); //Adds button js to TinyMCE
 add_filter( 'mce_buttons', 'dhivehi_text_register_buttons' ); //Registers button with TinyMCE
 
 //Add Shortcodes
-add_shortcode('dhivehi', 'dhivehi_text_shortcode'); //[dhivehi] shortcode
+add_shortcode( 'dhivehi', 'dhivehi_text_shortcode' ); //[dhivehi] shortcode
